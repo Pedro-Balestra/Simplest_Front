c43fa05cc129e661fd453043775f26c0a7889226
Pedro Balestra <pedro.balestra@outlook.com>
2023-08-04 23:33:55 -0300

    Criação da tela home

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 import 'package:simples_front_end/screens/home/menu_drawer/drawer.dart';
 import 'package:simples_front_end/utils/appColors.dart';
 
@@ -13,18 +15,50 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: AppColors.primaryColor,
+        child: const Icon(Icons.add, size: 25),
+      ),
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
       drawer: menuDrawer(context),
-      body: Container(
-        width: double.infinity,
-        height: 250,
-        decoration: BoxDecoration(
-          gradient: backgroundApp(),
-        ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 200,
+            decoration: BoxDecoration(
+              gradient: backgroundApp(),
+            ),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 25.0),
+                child: Text(
+                  "R 2362.60",
+                  style: GoogleFonts.poppins(
+                      fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height / 4,
+                    child: Lottie.asset('assets/lottie/profile_hello.json'),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
