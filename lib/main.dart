1765e8c363700f011976b45402a1b5b7d57162e2
Pedro Balestra <pedro.balestra@outlook.com>
2023-08-09 21:33:12 -0300

    Vinculando com o FireBase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:simples_front_end/screens/home/home.dart';
 import 'package:simples_front_end/screens/login/login_page.dart';
 import 'screens/signUp/signUp_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
