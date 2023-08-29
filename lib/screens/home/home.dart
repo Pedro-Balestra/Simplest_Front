2dfa982d7cd5f02a0de4ecfa542e4201e424f490
Pedro Balestra <pedro.balestra@outlook.com>
2023-08-28 21:26:31 -0300

    feat: Inicio da construção do gráfico

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,7 +3,7 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:lottie/lottie.dart';
+import 'package:simples_front_end/screens/home/graph/graph.dart';
 // import 'package:permission_handler/permission_handler.dart';
 import 'package:simples_front_end/screens/home/menu_drawer/drawer.dart';
 import 'package:simples_front_end/utils/appColors.dart';
@@ -16,35 +16,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  // FilePickerResult? result;
-  // String? _fileText;
-  // PlatformFile? pickedFile;
-  // bool isLoading = false;
-  // File? fileToDisplay;
-
-  // void pickFile() async {
-  //   try {
-  //     // Verificar e solicitar a permissão de leitura externa
-  //     var status = await Permission.storage.status;
-  //     if (!status.isGranted) {
-  //       await Permission.storage.request();
-  //       status = await Permission.storage.status;
-  //       if (!status.isGranted) {
-  //         print("Permission denied");
-  //         return;
-  //       }
-  //     }
-
-  //     FilePickerResult? result = await FilePicker.platform.pickFiles(
-  //       type: FileType.custom,
-  //       allowedExtensions: ['pdf'],
-  //     );
-
-  //     if (result == null) return;
-  //   } catch (e) {
-  //     print(e.toString());
-  //   }
-  // }
+  double total = 5764.75;
 
   @override
   Widget build(BuildContext context) {
@@ -78,26 +50,14 @@ class _HomeState extends State<Home> {
               child: Padding(
                 padding: const EdgeInsets.only(top: 25.0),
                 child: Text(
-                  "R 2362.60",
+                  "R\$ $total",
                   style: GoogleFonts.poppins(
                       fontSize: 40, fontWeight: FontWeight.bold),
                 ),
               ),
             ),
           ),
-          Expanded(
-            child: Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height / 4,
-                    child: Lottie.asset('assets/lottie/profile_hello.json'),
-                  )
-                ],
-              ),
-            ),
-          ),
+          Graph(),
         ],
       ),
     );
